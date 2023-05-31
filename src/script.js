00366e7097c4d4a7d96d01934b0a244d9d7f8cd7
nicbar9 <130346871+nicbar9@users.noreply.github.com>
2023-05-31 15:20:15 +0800

    added relevant icon with API

--- src/script.js
+++ src/script.js
@@ -78,6 +78,12 @@ function displayWeatherCondition(response) {
 
   let description = document.querySelector(".descriptor");
   description.innerHTML = `${response.data.weather[0].main}`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(currentCity) {
